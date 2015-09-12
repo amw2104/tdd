1d6e6ac48aac4360551276c3a3dc938094690c81
Andrew Wilson <awilson@conductor.com>
2015-09-12 18:26:23 -0400

    Finished Chapter 12

--- lists/forms.py
+++ lists/forms.py
@@ -6,19 +6,6 @@ from lists.models import Item
 EMPTY_LIST_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
-class ExistingListItemForm(ItemForm):
-    
-    def __init__(self, for_list, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.instance.list = for_list
-
-    def validate_unique(self):
-        try:
-            self.instance.validate_unique()
-        except ValidationError as e:
-            e.error_dict = {'text' : [DUPLICATE_ITEM_ERROR]}
-            self._update_errors(e)
-
 class ItemForm(forms.models.ModelForm):
 
     class Meta:
@@ -38,3 +25,19 @@ class ItemForm(forms.models.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
+
+class ExistingListItemForm(ItemForm):
+
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text' : [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
